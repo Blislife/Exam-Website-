7b7301cd92a464870cf8be70c2a3eccd1ac7e852
JustinDaMilkman <jdominy42@gmail.com>
2024-10-30 19:30:16 -0700

    Delete django_auth/registrywebsite directory

--- django_auth/registrywebsite/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class studentNames(models.Model):
-    firstName = models.CharField(max_length=80)
-    lastName = models.CharField(max_length=80)
-
-    def __str__(self):
-        return self.firstName, self.lastName
